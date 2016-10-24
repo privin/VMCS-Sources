852da6e8fa335be22ce3f3a1d166d41a6b4de09f
Long Nguyen <e0013276@u.nus.edu>
2016-10-24 23:32:42 +0700

    Added test classes

--- tests/sg/edu/nus/iss/vmcs/store/AllTests.java
+++ tests/sg/edu/nus/iss/vmcs/store/AllTests.java
@@ -14,15 +14,15 @@ import junit.framework.TestSuite;
 public class AllTests {
 	public static TestSuite suite() throws Exception {
 		  TestSuite suite=new TestSuite("Store Tests");		  
-//		  suite.addTestSuite(CashStoreItemTest.class);
-//		  suite.addTestSuite(CashStoreTest.class);
-//		  suite.addTestSuite(CoinTest.class);
-//		  suite.addTestSuite(DrinksBrandTest.class);
-//		  suite.addTestSuite(DrinksStoreItemTest.class);
-//		  suite.addTestSuite(DrinksStoreTest.class);
-//		  suite.addTestSuite(StoreControllerTest.class);
-//		  suite.addTestSuite(StoreItemTest.class);
-//		  suite.addTestSuite(StoreObjectTest.class);
+		  suite.addTestSuite(CashStoreItemTest.class);
+		  suite.addTestSuite(CashStoreTest.class);
+		  suite.addTestSuite(CoinTest.class);
+		  suite.addTestSuite(DrinksBrandTest.class);
+		  suite.addTestSuite(DrinksStoreItemTest.class);
+		  suite.addTestSuite(DrinksStoreTest.class);
+		  suite.addTestSuite(StoreControllerTest.class);
+		  suite.addTestSuite(StoreItemTest.class);
+		  suite.addTestSuite(StoreObjectTest.class);
 		  return suite;
 	}
 }//End of class AllTests
